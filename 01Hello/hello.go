ed507ea032ec6827b16ba1fb0d22eef7962edb73
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-03-27 12:21:30 +0530

    created go basic project

---
+++ 01Hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	lang := "This is variablee in GO Language"
+	sum := 4 + 2
+	fmt.Println("Welcomwe to GO language")
+	fmt.Println(lang)
+	fmt.Print("The sum f 4 + 2 is : ", sum)
+
+}
